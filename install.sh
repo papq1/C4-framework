dee72e9db5440922c1f3b8882edd9e3635990ad5
papq1 <101420333+papq1@users.noreply.github.com>
2024-07-26 23:19:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#sudo apt-get update -y
-#sudo apt-get upgrade -y
-#sudo apt-get install make -y
-#sudo apt-get install golang-go -y
-#sudo snap install docker
-#sudo git clone https://github.com/its-a-feature/Mythic.git
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install make -y
+sudo apt-get install golang-go -y
+sudo snap install docker
+sudo git clone https://github.com/its-a-feature/Mythic.git
 cd /home/ubuntu/Mythic/  && sudo ./install_docker_ubuntu.sh
 cd /home/ubuntu/Mythic/Mythic_CLI/ && sudo  make
 sudo cp /home/ubuntu/Mythic/Mythic_CLI/mythic-cli /home/ubuntu/Mythic
