faa4ca4131ee0787fd5f6cd7e686af1f9d0265e8
papq1 <101420333+papq1@users.noreply.github.com>
2024-07-27 22:48:18 +0200

    Create evilginx-install.sh

---
+++ evilginx-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install make -y
+sudo apt-get install golang-go -y
+sudo snap install docker
+sudo apt-get install unzip -y
+git clone https://github.com/kgretzky/evilginx2.git
+cd /home/ubuntu/evilginx2 && sudo make
+cp /home/ubuntu/evilginx2/evilginx /home/ubuntu/
