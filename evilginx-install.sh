333698376d4f9e17dcee7d1a3969d807f61a117b
papq1 <101420333+papq1@users.noreply.github.com>
2024-07-28 11:37:41 +0200

    Update evilginx-install.sh

--- evilginx-install.sh
+++ evilginx-install.sh
@@ -6,6 +6,6 @@ sudo apt-get install golang-go -y
 sudo snap install docker
 sudo apt-get install unzip -y
 git clone https://github.com/kgretzky/evilginx2.git
+cd /home/ubuntu/evilginx2 && sudo make
 sudo systemctl disable systemd-resolved
 sudo systemctl stop systemd-resolved
-cd /home/ubuntu/evilginx2 && sudo make
